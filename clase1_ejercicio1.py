1afa0ae4c7102ec7c526cc3616b29a7c3380d96f
Ariadnamelgarejo1 <156713773+Ariadnamelgarejo1@users.noreply.github.com>
2024-01-15 16:05:44 -0400

    Add files via upload
    ejercicio inicial de python

---
+++ clase1_ejercicio1.py
@@ -0,0 +1,5 @@
+#suma de dos numeros
+numero_x = float(input("ingrese un numero "))
+numero_y = float(input("ingrese otro numero"))
+suma = numero_x + numero_y
+print("\nla suma es :", suma)
---
+++ clase1_ejercicio3.py
@@ -0,0 +1,5 @@
+#calculo de IMC
+peso = float(input("peso (kg)"))
+altura = float (input("altura(m):"))
+imc = peso / (altura**2)
+print("su imc es : ", imc)
